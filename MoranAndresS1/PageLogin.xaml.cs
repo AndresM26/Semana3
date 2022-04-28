18085255adf40850716039e0beaa9eed934e74be
cmoran <cmoran@LUIOHMAMORAN>
2022-04-27 20:55:52 -0500

    Agregar archivos de proyecto.

---
+++ MoranAndresS1/PageLogin.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoranAndresS1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageLogin : ContentPage
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+
+            String user = txtUser.Text;
+            String pass = txtPass.Text;
+
+            if ((user == "estudiante2021") && (pass == "uisrael2021"))
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+               DisplayAlert("Datos incorrectos", " Intentar nuevamente", "cerrar");
+
+            }
+
+
+        }
+    }
+}
